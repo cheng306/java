08fab5724ab9b7df771c04400f3095d3872f45fe
cheng306 <johnnycheng0306@gmail.com>
2022-01-24 10:35:29 -0500

    commit

--- Program.java
+++ Program.java
@@ -1,5 +1,39 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Program {
   public static void main(String[] args){
-    System.out.println("This is my first program in java");
+    Integer arr[] = new Integer[4];
+    arr[0] = 0;
+    arr[1] = 0;
+    arr[2] = 1;
+    arr[3] = 2;
+
+    Arrays.sort(arr, new Comparator<Integer>(){
+      public int compare(Integer a, Integer b){
+        return b-a;
+      }
+    });
+
+    Arrays.sort(arr, (Integer a, Integer b)->{return b-a;});
+   
+
+    //compress(3, arr);
+    System.out.println(arr[0]);
+    System.out.println(arr[1]);
+    System.out.println(arr[2]);
+    System.out.println(arr[3]);
+    
   }//End of main
-}//End of FirstJavaProgram Class
+
+
+    public static int compress(int a, int[] arr){
+      if (arr[a] == a){
+        return a;
+      }
+      return arr[a] = compress(arr[a], arr);
+    }
+  
+  
+  
+}
